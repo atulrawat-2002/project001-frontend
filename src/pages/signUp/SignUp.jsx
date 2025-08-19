47c9e9962ad8cd31d783439ae90076abd76f1b79
Atul <dr6217811@gmail.com>
2025-08-19 17:24:59 +0530

    fixing for deploymen

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import "./signUp.scss"
+import "./SignUp.scss"
 import { useState } from "react";
 import { axiosClient } from "../../utils/axiosClient";
 import { TOAST_FAILUR, TOAST_SUCCESS } from "../../App";
